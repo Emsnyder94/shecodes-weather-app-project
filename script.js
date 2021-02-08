2c2b613bfe50d342f7be3508909cc9222396edcd
Emsnyder94 <76456667+Emsnyder94@users.noreply.github.com>
2021-02-08 10:45:42 -0500

    Udated date and time in js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+function formatDate(date) {
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+  let day = days[dayIndex];
+  return `${day} ${hours}:${minutes}`;
+}
+let dateElement = document.querySelector("#date");
+let now = new Date();
+dateElement.innerHTML = formatDate(now);
+
 function displayTemperature(response){
     let cityElement= document.querySelector("#city");
     let temperatureElement = document.querySelector("#city-temperature");
