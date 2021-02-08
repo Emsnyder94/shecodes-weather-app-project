02c6631a391fd899b8221960a5068511174ffef1
Emsnyder94 <76456667+Emsnyder94@users.noreply.github.com>
2021-02-08 01:35:44 -0500

    Deleted link

--- script.js
+++ script.js
@@ -1,48 +1,10 @@
-function formatDate(date) {
-  let hours = date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-  let dayIndex = date.getDay();
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday"
-  ];
-  let day = days[dayIndex];
-  return `${day} ${hours}:${minutes}`;
+function displayTemperature(response){
+    console.log(response.data.main.temp);
+    let temperatureElement = document.querySelector("#city-temperature");
+    temperatureElement.innerHTML = Math.round(response.data.main.temp);
 }
-let dateElement = document.querySelector("#current-date");
-let now = new Date();
-dateElement.innerHTML = formatDate(now);
-let searchCity = document.querySelector("#search-city");
-searchCity.addEventListener("submit", citySearch);
-//HW 5
-function showTemp(response) {
-  console.log(response.data);
-  document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
-  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
-  document.querySelector("#wind").innerHTML = response.data.wind.speed;
-}
-function search(city) {
-  let apiKey = "0a41317b31ebed3d5245fc696b6b07f2";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
-}
-function citySearch(event) {
-  event.preventDefault();
-  let city = document.querySelector("#city-input").value;
-  search(city);
-}
-search("New York");
+
+    let apiKey="0a41317b31ebed3d5245fc696b6b07f2";
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid={apiKey}&units=metrics`;
+
+    axios.get(apiUrl).then(displayTemperature);
