6e703586e27ab1a277384414163b719d83c121a3
Emsnyder94 <76456667+Emsnyder94@users.noreply.github.com>
2021-02-08 10:02:42 -0500

    Added city element to js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function displayTemperature(response){
-    console.log(response.data.main.temp);
+    let cityElement= document.querySelector("#city");
     let temperatureElement = document.querySelector("#city-temperature");
-    temperatureElement.innerHTML = Math.round(response.data.main.temp);
+    temperatureElement.innerHTML = Math.round(response.data);
+    cityElement.innerHTML = response.data.name;
 }
 
     let apiKey="0a41317b31ebed3d5245fc696b6b07f2";
-    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid={apiKey}&units=metrics`;
+    let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metrics`;
 
     axios.get(apiUrl).then(displayTemperature);
